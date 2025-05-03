ea212f15efd328570f7cf746d3f996fdfc8ea189
Dilip Yadav <codingiitiandilip@gmail.com>
2025-05-03 10:54:54 +0530

    [sql] : drop & truncate

---
+++ 01_week/07_drop_truncate.sql
@@ -0,0 +1,16 @@
+show databases;
+
+use week_01;
+
+show tables;
+
+-- -----truncate table table_name;-----
+
+
+select * from only_name;
+TRUNCATE table only_name;
+
+SELECT * from only_name;
+
+----DROP TABLE table_name;----
+DROP TABLE only_name;
